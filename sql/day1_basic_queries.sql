06cc87e91fd9015326c8597a157b594a5df87a3e
Srilatha Asu <srilathareddyasu@gmail.com>
2026-01-21 18:03:42 -0500

    Update day1_basic_queries.sql

--- sql/day1_basic_queries.sql
+++ sql/day1_basic_queries.sql
@@ -1,1 +1,23 @@
+--1. Total revenue per customer
+SELECT customer_id, SUM(amount) AS total_revenue FROM orders  GROUP BY customer_id;
+
+--2. Total revenue by city
+SELECT c.city, SUM(o.amount) AS total_revenue FROM customers c
+JOIN orders o ON c.customer_id =o.customer_id GROUP BY c.city
+
+--3. Customers with revenue greater than 10,000
+SELECT customer_id, SUM(amount) AS total_revenue FROM orders
+GROUP BY customer_id
+HAVING SUM(amount) >10000
+
+--4. Orders sorted by highest amount
+SELECT *
+FROM orders
+ORDER BY amount DESC;
+
+--5. Daily total revenue
+SELECT order_date, SUM(amount) AS daily_revenue
+FROM orders
+GROUP BY order_date
+ORDER BY order_date;
 
